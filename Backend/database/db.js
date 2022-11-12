932d5a43c31dfe29fac4ad7078cbe45efbdec018
coderkishornimje <96238959+coderkishornimje@users.noreply.github.com>
2022-11-12 18:28:23 +0530

    Update db.js

--- Backend/database/db.js
+++ Backend/database/db.js
@@ -1,3 +1,7 @@
+// module.exports = {
+//   db: 'mongodb://localhost:27017/reactdb'
+// };
+
 module.exports = {
-  db: 'mongodb://localhost:27017/reactdb'
+  db: 'mongodb+srv://kishornimje:coderkishor@cluster0.ippqtzw.mongodb.net/?retryWrites=true&w=majority'
 };
